b67bcba74095a7d7383c18f0773f07ca2f8b6068
Ayyappa818 <ayyappa666swamy@gmail.com>
2024-09-13 19:12:49 +0530

    changes

---
+++ React/players.js
@@ -0,0 +1,68 @@
+var players = [
+    {
+      "fullname": "Rahul chaudary",
+      "image": "https://dtnext-prod.s3.ap-south-1.amazonaws.com/imported/import/Images/Article/201710040130428512_Not-thinking-of-personal-milestones-says-Rahul_SECVPF.gif",
+      "rating": "5 Star player",
+      "review": "Great product, highly recommended!"
+    },
+    {
+      "fullname": "Pardeep Narwal",
+      "image": "https://www.sportsadda.com/static-assets/waf-images/4e/f6/a1/16-9/bCFmyZm0eh.jpg?v=1.6&w=1024",
+      "rating": 3.8,
+      "review": "Good product, but could be better."
+    },
+    {
+      "fullname": "Ajay thakur",
+      "image": "https://staticg.sportskeeda.com/editor/2018/10/1902c-15388255586699-500.jpg",
+      "rating": 5.0,
+      "review": "Absolutely love this product!"
+    },
+    {
+      "fullname": "Manjeet chhillar",
+      "image": "https://pbs.twimg.com/media/CNLIsuUUAAAj69T.png",
+      "rating": 2.5,
+      "review": "Not satisfied with the product."
+    },
+    {
+      "fullname": "Deepak hooda",
+      "image": "https://s3.ap-south-1.amazonaws.com/assets-pkl.sportz.io/waf-images/5b/55/3b/16-9/4KyEjpgNLN.jpg",
+      "rating": 4.0,
+      "review": "Decent product, could be improved."
+    },
+    {
+      "fullname": "Dharmaraj cheralathan",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDpcqESKJ1f-urlx7tvFOZOYJmTa-TKLsnKR-nGS8uU1jxHpChUiN97bFWwYiiuYko6zI&usqp=CAU",
+      "rating": 3.5,
+      "review": "Mixed feelings about the product."
+    },
+    {
+      "fullname": "Anup Kumar",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9rCIaZFGeIC-yBuF18IqrD0BBXRYMKj2NpQ&s",
+      "rating": 4.8,
+      "review": "Excellent product, exceeded expectations."
+    },
+    {
+      "fullname": "Maninder Singh",
+      "image": "https://a2.espncdn.com/combiner/i?img=%2Fphoto%2F2022%2F1112%2Fr1089850_1296x729_16%2D9.jpg",
+      "rating": 2.0,
+      "review": "Very disappointed with the product."
+    },
+    {
+      "fullname": "Rohit kumar",
+      "image": "https://media.ahmedabadmirror.com/am/uploads/mediaGallery/image/1616852837987.jpg-org",
+      "rating": 5.0,
+      "review": "Best product I've ever owned!"
+    },
+    {
+      "fullname": "Meraj sheykh",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOA6tYgo9_lGe5fPdjRWtiKS6yhlf8KgFLtMfWcAav7jSBp-BxScPXuASVLD97GCldHvQ&usqp=CAU",
+      "rating": 3.0,
+      "review": "Average product, nothing special."
+    },
+    {
+        "fullname": "Fazel Atrachali",
+        "image": "https://data1.ibtimes.co.in/en/full/722375/fazel-atrachali.jpg",
+        "rating": 5.0,
+        "review": "Best product I've ever owned!"
+      },
+  ]
