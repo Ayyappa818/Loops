2ff1821dbf9a4eff718f1e49e963fb5ffc7ac365
Ayyappa818 <ayyappa666swamy@gmail.com>
2024-09-16 19:06:25 +0530

    changes

--- React/players.js
+++ React/players.js
@@ -3,66 +3,66 @@ var players = [
       "fullname": "Rahul chaudary",
       "image": "https://dtnext-prod.s3.ap-south-1.amazonaws.com/imported/import/Images/Article/201710040130428512_Not-thinking-of-personal-milestones-says-Rahul_SECVPF.gif",
       "rating": "5 Star player",
-      "review": "Great product, highly recommended!"
+      "review": "Rahul Chaudhari (born 16 June 1993) is an Indian Kabaddi player, who has played as a defender and later became a magnificent raider. He was the first player ever to score 500, 700 and 800 raid points in Pro Kabaddi League."
     },
     {
       "fullname": "Pardeep Narwal",
       "image": "https://www.sportsadda.com/static-assets/waf-images/4e/f6/a1/16-9/bCFmyZm0eh.jpg?v=1.6&w=1024",
       "rating": "5 Star raider",
-      "review": "Good product, but could be better."
+      "review": "Widely regarded as the greatest raider in kabaddi history, he led the Patna Pirates to three straight titles and holds a multitude of the league's raiding records. Pardeep Narwal. Dubki King, Record Breaker. Kabaddi Player."
     },
     {
       "fullname": "Ajay thakur",
       "image": "https://staticg.sportskeeda.com/editor/2018/10/1902c-15388255586699-500.jpg",
       "rating": "5 Sart raider",
-      "review": "Absolutely love this product!"
+      "review": "Ajay Thakur is an Indian professional Kabaddi player. And is the former captain of the Indian National Kabaddi team. He is one of the “Best Indian Raiders” of all time"
     },
     {
       "fullname": "Manjeet chhillar",
       "image": "https://pbs.twimg.com/media/CNLIsuUUAAAj69T.png",
-      "rating": "4 Star raider",
-      "review": "Not satisfied with the product."
+      "rating": "5 star Defender",
+      "review": "Manjeet Chhillar, nicknamed One Man Army, He is a professional Kabaddi Player and a vital part of the Indian National Kabaddi team. He has also captained Bengaluru Bulls in the very first season ."
     },
     {
       "fullname": "Deepak hooda",
       "image": "https://s3.ap-south-1.amazonaws.com/assets-pkl.sportz.io/waf-images/5b/55/3b/16-9/4KyEjpgNLN.jpg",
-      "rating": "4 star raider",
-      "review": "Decent product, could be improved."
+      "rating": "5 Star All-rounder",
+      "review": "Deepak Ram Niwas Hooda (born 10 June 1994) is an Indian professional Kabaddi player and the former captain of India national kabaddi team. He was part of the Indian team which won gold medal at the 2016 South Asian Games."
     },
     {
       "fullname": "Dharmaraj cheralathan",
       "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDpcqESKJ1f-urlx7tvFOZOYJmTa-TKLsnKR-nGS8uU1jxHpChUiN97bFWwYiiuYko6zI&usqp=CAU",
-      "rating": 3.5,
-      "review": "Mixed feelings about the product."
+      "rating": "5 Star Defender",
+      "review": "He specializes in Block and Bear hug, making him one of the strongest players in the sport and a nightmare of the raiders."
     },
     {
       "fullname": "Anup Kumar",
       "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9rCIaZFGeIC-yBuF18IqrD0BBXRYMKj2NpQ&s",
-      "rating": 4.8,
-      "review": "Excellent product, exceeded expectations."
+      "rating": "5 Star raider",
+      "review": "Anup Kumar, the former captain of the Indian Kabaddi team, rose to fame with his exceptional skills on the mat. A soft-spoken leader, he was known for his agility, quick decision-making abilities, and his calm demeanor."
     },
     {
       "fullname": "Maninder Singh",
       "image": "https://a2.espncdn.com/combiner/i?img=%2Fphoto%2F2022%2F1112%2Fr1089850_1296x729_16%2D9.jpg",
-      "rating": 2.0,
-      "review": "Very disappointed with the product."
+      "rating": "5 Star raider",
+      "review": "There are ultra-strong kabaddi players. And then there is Maninder Singh. He's got boulders for shoulders, logs for quadriceps and is an absolute tank on the mat. A few days ago, he lifted an opposition defender over the midline and into his side of the court."
     },
     {
       "fullname": "Rohit kumar",
       "image": "https://media.ahmedabadmirror.com/am/uploads/mediaGallery/image/1616852837987.jpg-org",
-      "rating": 5.0,
-      "review": "Best product I've ever owned!"
+      "rating": "4 Star raider",
+      "review": " Bengaluru Bulls needed to score quick in the closing stages and that prompted captain Rohit Kumar to improvise. After an audacious-looking effort, Renowned for his leadership acumen."
     },
     {
       "fullname": "Meraj sheykh",
       "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOA6tYgo9_lGe5fPdjRWtiKS6yhlf8KgFLtMfWcAav7jSBp-BxScPXuASVLD97GCldHvQ&usqp=CAU",
-      "rating": 3.0,
-      "review": "Average product, nothing special."
+      "rating": "5 star All-rounder",
+      "review": "Meraj was even hailed as a do-or-die raid specialist in the Delhi setup for his guile, agility and ability to perform under pressure on the mat. However, despite a good start to his PKL career."
     },
     {
         "fullname": "Fazel Atrachali",
         "image": "https://data1.ibtimes.co.in/en/full/722375/fazel-atrachali.jpg",
-        "rating": 5.0,
-        "review": "Best product I've ever owned!"
+        "rating": "5 Star defender",
+        "review": "Fazel Atrachali, one of the most feared defenders in the Pro Kabaddi League (PKL), is also the most successful defender in PKL history now"
       },
   ]
